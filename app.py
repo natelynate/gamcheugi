82f2d83c9bd2e34e23bb3c954e122ea93d74c410
natelynate <nately2nate@gmail.com>
2024-02-02 17:02:48 +0900

    Updated app.py autoload db config when running flask app

--- app.py
+++ app.py
@@ -6,7 +6,5 @@ app = Flask(__name__)
 def main():
     return f"<p>Hello, Flask! with debugger mode active</p> <br> <p>Database connection is {'working'}</p>"
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    

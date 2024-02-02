45646c86405c092bb509b01a5c0d93f752ee6f18
natelynate <nately2nate@gmail.com>
2024-02-02 17:21:47 +0900

    Added try-except in get_db_conn function

--- logics/db.py
+++ logics/db.py
@@ -6,12 +6,15 @@ from flask import current_app, g
 
 def get_db_conn():
     """Returns new/existing DB connection object for a request"""
-    if 'db' not in g:
-        g.db = pymysql.connect(
-            host=current_app.config['DB_HOST'],
-            user=current_app.config['DB_USER'],
-            password=current_app.config['DB_PASSWORD'],
-            database=current_app.config['DB_NAME'],
-            cursorclass=pymysql.cursors.DictCursor
-        )
-    return g.db
+    try:
+        if 'db' not in g:
+            g.db = pymysql.connect(
+                host=current_app.config['DB_HOST'],
+                user=current_app.config['DB_USER'],
+                password=current_app.config['DB_PASSWORD'],
+                database=current_app.config['DB_NAME'],
+                cursorclass=pymysql.cursors.DictCursor
+            )
+        return g.db
+    except:
+        return "Error"

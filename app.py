6cf682d9e0f64655521d146d2b378c229126bcb6
natelynate <nately2nate@gmail.com>
2024-02-02 17:22:11 +0900

    added a db conn check when sending get to base url

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from settings.config import Config
+from logics.db import get_db_conn
 
 app = Flask(__name__)
 
 @app.get("/")
 def main():
-    return f"<p>Hello, Flask! with debugger mode active</p> <br> <p>Database connection is {'working'}</p>"
+    is_dbconn = get_db_conn()
+    return f"<p>Hello, Flask! with debugger mode active</p> <br> <p>Database connection established:{str(is_dbconn)}</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

412967eda5114572b78ff2a22708f1b45a4bb3b5
natelynate <nately2nate@gmail.com>
2024-02-02 17:03:28 +0900

    Created a get_db_conn() that returns mysql connection object for request

--- logics/db.py
+++ logics/db.py
@@ -2,5 +2,16 @@
 
 import pymysql
 from settings.config import *   
+from flask import current_app, g
 
-
+def get_db_conn():
+    """Returns new/existing DB connection object for a request"""
+    if 'db' not in g:
+        g.db = pymysql.connect(
+            host=current_app.config['DB_HOST'],
+            user=current_app.config['DB_USER'],
+            password=current_app.config['DB_PASSWORD'],
+            database=current_app.config['DB_NAME'],
+            cursorclass=pymysql.cursors.DictCursor
+        )
+    return g.db

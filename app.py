aec643bee274f8a7b238ffabe06fcc0fc17bce75
natelynate <nately2nate@gmail.com>
2024-02-02 17:05:28 +0900

    Updated app.py autoload db config when running flask app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from settings.config import Config
 
 app = Flask(__name__)
 
@@ -8,3 +9,4 @@ def main():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    app.config.from_object(Config)

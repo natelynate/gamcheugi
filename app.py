269d757bfede37017def2c6e8ea954ef7f82abd5
natelynate <nately2nate@gmail.com>
2024-02-02 17:37:15 +0900

    Fixed a db conn check function at app.py.

--- app.py
+++ app.py
@@ -3,12 +3,23 @@ from settings.config import Config
 from logics.db import get_db_conn
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.get("/")
 def main():
-    is_dbconn = get_db_conn()
-    return f"<p>Hello, Flask! with debugger mode active</p> <br> <p>Database connection established:{str(is_dbconn)}</p>"
+    if get_db_conn():
+        is_dbconn = True
+    conn = get_db_conn()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM sample_table")
+    result = cursor.fetchall()[0]
+    cursor.close()
+    conn.close()
+    return f"""<p>Hello, Flask! with debugger mode active</p> <br> 
+        <p>Database connection established:{str(is_dbconn)}</p>
+        <br> <p>{result['DATA']}</p>"""
 
 if __name__ == '__main__':
+    
     app.run(host='0.0.0.0', port=5000)
-    app.config.from_object(Config)
+    
